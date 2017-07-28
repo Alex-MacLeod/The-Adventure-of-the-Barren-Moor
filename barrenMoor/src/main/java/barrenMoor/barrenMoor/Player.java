7554f14a69c74f6c0102728385ed76fafe4fba86
AlexMacLeod23 <30347591+AlexMacLeod23@users.noreply.github.com>
2017-07-28 09:37:05 +0100

    Add files via upload

---
+++ barrenMoor/src/main/java/barrenMoor/barrenMoor/Player.java
@@ -0,0 +1,10 @@
+package barrenMoor.barrenMoor;
+
+public class Player {
+	int[] featureCoords;
+	boolean victoryStatus;
+		public Player(int[] featureCoords, boolean victoryStatus) {
+			this.featureCoords = featureCoords;
+			this.victoryStatus = victoryStatus;
+		}
+}
---
+++ barrenMoor/src/test/java/barrenMoor/barrenMoor/barrenMoorTest.java
@@ -0,0 +1,7 @@
+package barrenMoor.barrenMoor;
+
+import junit.framework.TestCase;
+
+public class barrenMoorTest extends TestCase {
+
+}
