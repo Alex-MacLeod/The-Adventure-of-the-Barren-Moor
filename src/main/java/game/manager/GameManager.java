a526a9d0e1491582fc490a95e72f960b6e59a679
Alexander MacLeod <30347591+AlexMacLeod23@users.noreply.github.com>
2018-07-07 22:10:47 +0100

    Refactored GameManager

--- src/main/java/game/manager/GameManager.java
+++ src/main/java/game/manager/GameManager.java
@@ -1,14 +1,18 @@
 package game.manager;
 
 import game.events.Event;
+import game.events.Treasure;
 import game.player.Player;
 import game.utils.Directions;
 import game.utils.Input;
 
-import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class GameManager {
+
+    Random randomNumberGenerator = new Random();
 
 	public int[] createPoint(int[] point) {
 		if ((point[0] == 0 && point[1] == 0) || Math.abs(point[0]) > 3 || Math.abs(point[1]) > 3) {
@@ -28,7 +32,7 @@ public class GameManager {
 	}
 	
 	public float calculateDistance(int[] point) {
-		return (float) Math.sqrt(point[0]*point[0] + point[1]*point[1]);
+		return (float) Math.sqrt((double)point[0]*point[0] + (double)point[1]*point[1]);
 	}
 
 	public Enum<Directions> findDirection(int[] point) {
@@ -81,10 +85,10 @@ public class GameManager {
 		System.out.println("The display reads: " + calculateDistance(point) + " miles.");
 	}
 
-	public void startEvent(HashMap<Integer, Event> H, Player player) {
-		int random1to3 = 1 + (int)Math.round(Math.random())*2;
-		Event event = H.get(random1to3);
-		if (event.getClass().getSimpleName().equals("Treasure")) {
+	public void startEvent(Map<Integer, Event> eventMap, Player player) {
+		int random1to3 = 1 + randomNumberGenerator.nextInt(2);
+		Event event = eventMap.get(random1to3);
+		if (event instanceof Treasure) {
 			System.out.println("You have found a great oak chest filled with treasure!");
 		}
 		player.addPoints(event.getValue());
