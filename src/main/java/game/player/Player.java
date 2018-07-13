8c86833bc6c15cfddacd4225b4a522ab9ff25f8c
Alexander MacLeod <30347591+AlexMacLeod23@users.noreply.github.com>
2018-07-13 23:37:47 +0100

    Added wins, decideNextAction methods, removed checkScore

--- src/main/java/game/player/Player.java
+++ src/main/java/game/player/Player.java
@@ -1,10 +1,14 @@
 package game.player;
 
+import game.manager.Location;
+import game.util.Input;
+
 public class Player {
 
-	private String name;
+	private final String name;
 	private int score;
-	private boolean victorious;
+
+    private boolean victorious;
 
 	public Player(String name) {
 		this.score = 0;
@@ -28,10 +32,47 @@ public class Player {
 		return victorious;
 	}
 
-	public void checkScore() {
-		if (this.score >= 10000) {
-			this.victorious = true;
-		}
-		System.out.println("You now have " + this.score + " points");
-	}
+    public void wins() {
+        this.victorious = true;
+    }
+
+    public void decideNextAction() {
+        System.out.println("What are you going to do?");
+        String commandInput = Input.scan.nextLine();
+        String[] command = commandInput.split(" ");
+
+        switch (command[0]) {
+            case "check":
+                try {
+                    if ("score".equalsIgnoreCase(command[1])) {
+                        System.out.println("Your current score is " + this.score);
+                    } else if ("compass".equalsIgnoreCase(command[1])) {
+                        System.out.println("You take a moment to check the device.");
+                        System.out.println("The compass is pointing " + Location.findDirection() + ".");
+                        System.out.println("The display reads: " + Location.calculateDistance() + " miles.");
+                    } else {
+                        System.out.println("Invalid arguments, can only check compass or score");
+                    }
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Invalid arguments, must specify what you are checking: score or compass");
+                }
+
+                break;
+            case "walk":
+                try {
+                    Location.walk(command[1]);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Invalid arguments, must specify a single direction: North, South, East, or West");
+                }
+                break;
+            case "help":
+                System.out.println("Valid commands: \"check\", \"walk\", \"help\"");
+                break;
+            default:
+                System.out.println("Did not recognise command \"" + command[0] + "\". Use the \"help\" command to see list of valid commands");
+                break;
+        }
+    }
+
+
 }
--- src/test/java/player/PlayerTest.java
+++ src/test/java/player/PlayerTest.java
@@ -3,22 +3,46 @@ package player;
 import game.player.Player;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class PlayerTest {
 
     @Test
-    public void checkScore_shouldSetVictoryIffOver10000Points() {
+    public void addPoints_shouldAdd10000ToPlayerScore() {
         Player testPlayer1 = new Player("David");
-        Player testPlayer2 = new Player ("Procopis");
+
+        assertEquals(0, testPlayer1.getScore());
+
         testPlayer1.addPoints(10000);
-        testPlayer2.addPoints(5000);
-
-        testPlayer1.checkScore();
-        assertTrue(testPlayer1.isVictorious());
-
-        testPlayer2.checkScore();
-        assertFalse(testPlayer2.isVictorious());
+        assertEquals(10000, testPlayer1.getScore());
     }
+
+    @Test
+    public void addPoints_shouldAddAnyValueToPlayerScore() {
+        int pointsToAdd = 5000;
+        Player testPlayer2 = new Player ("Procopis");
+
+        assertEquals(0, testPlayer2.getScore());
+
+        testPlayer2.addPoints(pointsToAdd);
+        assertEquals(pointsToAdd, testPlayer2.getScore());
+    }
+
+    @Test
+    public void wins_shouldSetVictoriousToTrue() {
+        Player testPlayer3 = new Player("Deaglan");
+
+        assertFalse(testPlayer3.isVictorious());
+
+        testPlayer3.wins();
+        assertTrue(testPlayer3.isVictorious());
+    }
+
+//    @Test
+//    public void decideNextAction_should() {
+//
+//    }
+
 }
