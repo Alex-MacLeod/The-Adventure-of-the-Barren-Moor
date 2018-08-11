df1aceadd175aad0eb87d94bc5b869190da0e377
Alexander MacLeod <30347591+AlexMacLeod23@users.noreply.github.com>
2018-08-11 12:53:31 +0100

    Moved intro text to method

--- src/main/java/game/RunGame.java
+++ src/main/java/game/RunGame.java
@@ -22,16 +22,14 @@ public class RunGame {
 		String inputName = Input.scan.nextLine();
 		Player player = new Player(inputName);
 
-		Introduction introduction = new Introduction();
-
 		List<Event> eventList = new ArrayList<>();
 		eventList.add(new SmallTreasure());
 		eventList.add(new SmallTreasure());
 		eventList.add(new BigTreasure());
 
-		introduction.play();
-
 		Destination destination = new Destination();
+
+		introduction();
 
 		do {
 			decideNextAction(player, destination);
@@ -65,6 +63,21 @@ public class RunGame {
 		} else {
 			System.out.println("Bad luck, " + player.getName() + "! You have lost the game");
 		}
+	}
+
+	private static void introduction() {
+		System.out.println("You awaken to find yourself alone, lying cold and wet on a barren moor.");
+		System.out.println("Slowly, you pick yourself up from the ground and take in your bleak surroundings.");
+		System.out.println("All around, you are surrounded by a thick grey fog. You cannot see much around you.");
+		System.out.println("You can just about see a black weathered rock in front of you, struggling against"
+				+ " being swallowed up by the fog.");
+		System.out.println("You feel something strange in your pocket.");
+
+		System.out.println("You take it out and examine it.");
+		System.out.println("It looks a bit like a pocket watch. However, there is only one hand, "
+				+ "and it has a small display.");
+		System.out.println("The markings on it suggest that it might be a compass, but it definitely doesn't point north.");
+		System.out.println("The display seems to show a distance. Is this device pointing you towards something?");
 	}
 
 	private static void decideNextAction(Player player, Destination destination) {
