342dc01a91f7bdb89664e4253e41256f26ccd581
Alexander MacLeod <30347591+AlexMacLeod23@users.noreply.github.com>
2018-08-13 18:21:20 +0100

    Renamed test file

--- src/test/java/events/TreasureTest.java
+++ src/test/java/events/TreasureTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class EventTest {
+public class TreasureTest {
 
     @Test
     public void playEvent_shouldSetEventAsCompleted(){
